3aa1e995373ffb1de7b5764a5a6b38d964930a20
Dobbymin <gmin990624@gmail.com>
2025-06-03 17:22:01 +0900

    feat: router 경로 상수화

---
+++ src/shared/constants/router-path.ts
@@ -0,0 +1,11 @@
+export const ROUTER_PATH = {
+  ROOT: '/',
+  COMBINATIONS: '/combinations',
+  COMBINATIONS_DETAIL: '/combinations/:id',
+  COMBO_LIST: '/combo-list',
+  COMBO_DETAIL: '/combo-detail/:id',
+  COMMUNITY: '/community',
+  EVENT: '/event',
+  MENU: '/menu',
+  NEARBY: '/nearby',
+};
