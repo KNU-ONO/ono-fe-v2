31f251b72d2a3e66df7e7ef1d12d0ed8a6b2444d
Dobbymin <gmin990624@gmail.com>
2025-06-03 23:44:11 +0900

    feat: msw 도입 및 적용

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,19 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+async function enableMocking() {
+  if (!import.meta.env.DEV) {
+    return;
+  }
+  const { worker } = await import('./shared/mocks/browser.ts');
+
+  return worker.start();
+}
+
+enableMocking().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+});
