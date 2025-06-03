ab6857b607b49f6d826d6141882eb754d405802c
Dobbymin <gmin990624@gmail.com>
2025-06-04 01:12:06 +0900

    refactor(community): dynamic path 적용

--- src/features/community/components/button/AddPostButton.tsx
+++ src/features/community/components/button/AddPostButton.tsx
@@ -2,12 +2,14 @@ import { useNavigate } from 'react-router-dom';
 
 import { Plus } from 'lucide-react';
 
+import { ROUTER_PATH } from '@/shared';
+
 export const AddPostButton = () => {
   const navigate = useNavigate();
 
   return (
     <button
-      onClick={() => navigate('/community/create')}
+      onClick={() => navigate(ROUTER_PATH.COMMUNITY_CREATE)}
       className='absolute right-2 bottom-2 flex h-[65px] w-[65px] items-center justify-center rounded-full bg-red-600'
     >
       <Plus className='h-8 w-8 text-white' />
