9653654cad332018a297c80580e35691eca93ca3
Dobbymin <gmin990624@gmail.com>
2025-06-15 19:28:57 +0900

    refactor(community): 탭 UI 수정

--- src/features/community/ui/section/ComboTabSection.tsx
+++ src/features/community/ui/section/ComboTabSection.tsx
@@ -1,3 +1,4 @@
+import { TabButton } from '../../components';
 import { TAB_DATA } from '../../data';
 
 type Props = {
@@ -6,33 +7,28 @@ type Props = {
 };
 
 export const ComboTabSection = ({ activeTab, setActiveTab }: Props) => {
+  const activeTabIndex = TAB_DATA.findIndex((tab) => tab === activeTab);
+  const tabCount = TAB_DATA.length;
+
+  const underlineStyle = {
+    width: `${100 / tabCount}%`,
+    transform: `translateX(${activeTabIndex * 100}%)`,
+  };
+
   return (
-    <div className='relative h-[43px]'>
-      {TAB_DATA.map((tab, i) => (
-        <button
+    <div className='relative flex h-full w-full border-b py-2'>
+      {TAB_DATA.map((tab) => (
+        <TabButton
           key={tab}
-          onClick={() => setActiveTab(tab)}
-          className='absolute top-0 h-[27px] w-[44px] text-[15px] leading-[27px] font-medium text-black'
-          style={{
-            left:
-              i === 0 ? '40px' : i === 1 ? 'calc(50% - 22px - 1.5px)' : '289px',
-            fontWeight: activeTab === tab ? 600 : 400,
-          }}
-        >
-          {tab}
-        </button>
+          tab={tab}
+          activeTab={activeTab}
+          setActiveTab={setActiveTab}
+        />
       ))}
-      <div className='absolute top-[36px] w-full border-b border-[#DBDBDB]' />
+
       <div
-        className='absolute top-[34px] h-[2px] w-[126px] bg-[#B81E22] transition-all duration-500 ease-in-out'
-        style={{
-          left:
-            activeTab === '인기글'
-              ? '0px'
-              : activeTab === '꿀조합'
-                ? 'calc(50% - 63px)'
-                : 'calc(100% - 126px)',
-        }}
+        className='absolute bottom-[-1px] h-[2px] bg-ono-red-two transition-all duration-300 ease-in-out'
+        style={underlineStyle}
       />
     </div>
   );
